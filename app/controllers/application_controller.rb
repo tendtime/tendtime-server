92e351beaaae9524e80699284ebe9ea538f2103f
Tracey Norrish <tracey.norrish@abletech.co.nz>
2015-04-11 21:30:53 +1200

    fix sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 protected
   def after_sign_in_path_for(_resource)
     return products_path if current_user.supplier?
-    return project_test_path if current_user.architect?
+    return projects_path if current_user.architect?
     return admin_root_path if current_user.admin?
     project_test_path
   end
