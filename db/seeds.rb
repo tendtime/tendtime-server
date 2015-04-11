0412b3bb2ee5d766b2a69eb9c451ac7c4f1c3e2e
Tracey Norrish <tracey.norrish@abletech.co.nz>
2015-04-11 18:22:46 +1200

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,26 +14,46 @@ puts "nombre de user dans la DB: #{User.count}"
 Supplier.create(company_name: 'Mitre 10', contact_name: 'Jane Doe')
 
 Product.create(
+  family: "Rectangular Mullion",
+  title: "64 x 128 rectangular",
+  unit: "mm"
+)
+
+Product.create(
+  family: "System Panel",
+  title: "Glazed",
+  unit: "m2"
+)
+
+Product.create(
+  family: "Basic Roof",
+  title: "SG Metal panel roof",
+  unit: "m3"
+)
+
+Product.create(
   family: "Basic Wall",
   title: "SIP 202mm Wall - conc clad",
-  price_per_unit: 1000,
   unit: "m3"
 )
 Product.create(
   family: "Basic Wall",
   title: "Cavity waill sliders",
-  price_per_unit: 1000,
   unit: "m3"
 )
 Product.create(
   family: "Basic Wall",
   title: "Interior - Partition",
-  price_per_unit: 1000,
   unit: "m3"
 )
 Product.create(
   family: "Basic Wall",
   title: "Retaining - 300mm concrete",
-  price_per_unit: 1000,
+  unit: "m3"
+)
+
+Product.create(
+  family: "Curtain Wall",
+  title: "SH. Curtain Wall",
   unit: "m3"
 )
